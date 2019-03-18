a2d8b181b16045fd5bd1c2afa45eb807d04f479f
huangdingbo <hdb378969398>
2019-03-19 02:09:41 +0800

    监控预警api

--- api/controllers/WarningIndexController.php
+++ api/controllers/WarningIndexController.php
@@ -9,12 +9,18 @@
 namespace api\controllers;
 
 
+use api\models\ReadGradeModel;
 use api\models\WarningIndexModel;
 use yii\web\Controller;
 use yii\web\ForbiddenHttpException;
 
 class WarningIndexController extends MyController
 {
+    /**
+     * @return array
+     * @throws ForbiddenHttpException
+     * 预警首页头部
+     */
     public function actionHeader(){
         $postData = \Yii::$app->request->post();
         $grade =  isset($postData['grade']) ? $postData['grade'] : '';
@@ -36,6 +42,10 @@ class WarningIndexController extends MyController
         ];
     }
 
+    /**
+     * @return array
+     * 年级列表
+     */
     public function actionGradeList(){
         $list = WarningIndexModel::getGradeList();
 
@@ -44,6 +54,10 @@ class WarningIndexController extends MyController
         ];
     }
 
+    /**
+     * @return array
+     * 班级列表
+     */
     public function actionClassList(){
         $list = WarningIndexModel::getClassList();
 
@@ -51,4 +65,139 @@ class WarningIndexController extends MyController
             'list' => $list
         ];
     }
+
+    public function actionHighRisk(){
+        $postData = \Yii::$app->request->post();
+        $grade =  isset($postData['grade']) ? $postData['grade'] : '';
+        $type = isset($postData['type']) ? $postData['type'] : '';
+        $list = WarningIndexModel::getHighRiskStudent($grade,$type);
+        return [
+            'list' => $list
+        ];
+    }
+
+    public function actionWarningType(){
+        $postData = \Yii::$app->request->post();
+        $grade =  isset($postData['grade']) ? $postData['grade'] : '';
+        $type = isset($postData['type']) ? $postData['type'] : '';
+        $list = WarningIndexModel::getWarningTypeData($grade,$type);
+
+        return [
+            'list' => $list
+        ];
+    }
+
+    public function actionWarningWork(){
+        $list = WarningIndexModel::getWarningWorkList();
+
+        return [
+            'list' => $list
+        ];
+    }
+
+    public function actionWarningDevelop(){
+        $postData = \Yii::$app->request->post();
+        $grade =  isset($postData['grade']) ? $postData['grade'] : '';
+        $class = isset($postData['class']) ? $postData['class'] : '';
+        $type = isset($postData['type']) ? $postData['type'] : '';
+        $studentType = isset($postData['studentType']) ? $postData['studentType'] : '';
+        if (!$grade){
+            throw new ForbiddenHttpException('缺少年级');
+        }
+        if (!$studentType){
+            throw new ForbiddenHttpException('缺少类型,1理科2文科');
+        }
+
+        $list = WarningIndexModel::getWarningDevelopList($grade,$class,$type,$studentType);
+
+        return [
+            'list' => $list
+        ];
+    }
+
+    public function actionWarningStatistical(){
+        $postData = \Yii::$app->request->post();
+        $grade =  isset($postData['grade']) ? $postData['grade'] : '';
+        $class = isset($postData['class']) ? $postData['class'] : '';
+        $type = isset($postData['type']) ? $postData['type'] : '';
+        $studentType = isset($postData['studentType']) ? $postData['studentType'] : '';
+        $test = isset($postData['test']) ? $postData['test'] : '';
+        if (!$grade){
+            throw new ForbiddenHttpException('缺少年级');
+        }
+        if (!$studentType){
+            throw new ForbiddenHttpException('缺少类型,1理科2文科');
+        }
+        if (!$type){
+            throw new ForbiddenHttpException('缺少预警类型');
+        }
+
+        $list = WarningIndexModel::getWarningStatisticalList($grade,$class,$type,$studentType,$test);
+
+        return [
+            'list' => $list
+        ];
+    }
+
+    public function actionWarningAll(){
+        $postData = \Yii::$app->request->post();
+        $grade =  isset($postData['grade']) ? $postData['grade'] : '';
+        $class = isset($postData['class']) ? $postData['class'] : '';
+        $test = isset($postData['test']) ? $postData['test'] : '';
+        $type = isset($postData['type']) ? $postData['type'] : '';
+        $studentType = isset($postData['studentType']) ? $postData['studentType'] : '';
+        $course = isset($postData['course']) ? $postData['course'] : '';
+        $status = isset($postData['status']) ? $postData['status'] : '';
+        $nameStr = isset($postData['nameStr']) ? $postData['nameStr'] : '';
+
+        $list = WarningIndexModel::getWarningAllList($grade,$class,$test,$type,$studentType,$course,$status,$nameStr);
+
+        return [
+            'list' => $list
+        ];
+    }
+
+    public function actionWarningDetail(){
+        $postData = \Yii::$app->request->post();
+        $id =  isset($postData['id']) ? $postData['id'] : '';
+        if (!$id){
+            throw new ForbiddenHttpException('缺少预警id');
+        }
+        $list = WarningIndexModel::getWarningDetail($id);
+
+        return $list;
+    }
+
+    public function actionTestList(){
+        $postData = \Yii::$app->request->post();
+        $type = isset($postData['type']) ? $postData['type'] : '';
+        $grade =  isset($postData['grade']) ? $postData['grade'] : '';
+        if (!$type){
+            throw new ForbiddenHttpException('缺少学生类型，1理科2文科');
+        }
+        if (!$grade){
+            throw new ForbiddenHttpException('缺少年级');
+        }
+        $list = WarningIndexModel::getTestList($type,$grade);
+        return $list;
+    }
+
+   public function actionTypeList(){
+        $types = WarningIndexModel::$warningType;
+        $list = array();
+        foreach ($types as $key => $item){
+            $list[] = [
+                'name' => $item,
+                'value' => $key
+            ];
+        }
+        return $list;
+   }
+
+   public function actionCourseList(){
+       $postData = \Yii::$app->request->post();
+       $type = isset($postData['type']) ? $postData['type'] : '';
+
+       return ReadGradeModel::NumConfig($type);
+   }
 }
--- api/models/WarningIndexModel.php
+++ api/models/WarningIndexModel.php
@@ -12,6 +12,7 @@ namespace api\models;
 use frontend\models\Class0;
 use frontend\models\Grade;
 use frontend\models\Score;
+use frontend\models\Student;
 use frontend\models\Test;
 use frontend\models\Warning;
 use frontend\models\Wire;
@@ -72,5 +73,183 @@ class WarningIndexModel extends  Model
         }
 
         return $list;
+    }
+
+    public static function getHighRiskStudent($grade,$type){
+        $testNum = Test::find()->where(['grade_num' => $grade,'type' => $type])->count();
+        preg_match('/\d+/',$grade,$arr);
+        $studentList = Student::find()->where(['type'=>$type])
+            ->andWhere(['grade'=>$arr[0]])
+            ->asArray()
+            ->all();
+        $list = array();
+        foreach ($studentList as $item){
+            $num = Score::find()->leftJoin('warning','score.id = warning.score_id')
+                ->where(['score.student_id' => $item['student_id']])
+                ->count();
+            if ($num >= $testNum){
+                $banji = (Class0::findOne(['id'=>$item['banji']]))->name;
+                $list[] = [
+                    'name' => $item['name'],
+                    'studentId' => $item['student_id'],
+                    'gradeAndClass' => $item['grade'].'届'.$banji,
+                    'warningNum' => $num,
+                ];
+            }
+        }
+        return $list;
+    }
+
+    public static function getWarningTypeData($grade,$type){
+
+        $list = Score::find()->leftJoin('warning','score.id = warning.score_id')
+            ->select('count(*) as value,warning.type as name')
+            ->where(['score.grade' => $grade,'score.type' => $type])
+            ->groupBy('warning.type')
+//            ->createCommand()->getRawSql();
+            ->asArray()
+            ->all();
+        $sum = 0;
+        foreach ($list as $key=>&$item){
+            if (!$item['name']){
+                unset($list[$key]);
+                continue;
+            }
+            $sum += $item['value'];
+            $item['name'] = self::$warningType[$item['name']];
+        }
+
+        foreach ($list as &$item){
+            $item['ratio'] = (string)(round(($item['value']/$sum)*100,'2'));
+        }
+        return array_values($list);
+
+    }
+    public static $warningType = [
+        '1' => '成绩不合格学生',
+        '2' => '临界学生',
+        '3' => '学困学生',
+        '4' => '成绩下滑学生',
+        '5' => '偏科学生',
+    ];
+
+    public static function getWarningWorkList(){
+        $newTest = CommonModel::getNewTest();
+        $list = Warning::find()->leftJoin('test','test.test_num = warning.warning_test')
+            ->select('warning.id,warning.content as value,test.test_name as name')
+            ->where(['warning.warning_test' => $newTest['test_num']])
+            ->asArray()->all();
+        return $list;
+    }
+
+    public static function getWarningDevelopList($grade,$class,$type,$studentType){
+        $query = Warning::find()->leftJoin('score','score.id = warning.score_id')
+            ->where(['score.grade' => $grade,'score.type' => $studentType]);
+        if ($class){
+            $query->andWhere(['score.banji' => $class]);
+        }
+        if ($type){
+            $query->andWhere(['warning.type' => $type]);
+        }
+        $list = $query->select('count(*) as value,score.test_name as name')
+            ->groupBy('score.test_name')
+            ->orderBy('score.insert_time asc')
+            ->limit(10)
+            ->asArray()
+            ->all();
+       return $list;
+    }
+
+    public static function getWarningStatisticalList($grade,$class,$type,$studentType,$test){
+        $query = Warning::find()->leftJoin('score','score.id = warning.score_id')
+            ->select('warning.id,score.name,score.grade,score.banji,warning.content,warning.status')
+            ->where(['score.grade' => $grade,'score.type' => $studentType,'warning.type' => $type]);
+        if ($class){
+           $query->andWhere(['score.banji' => $class]);
+        }
+        if ($test){
+            $query->where(['score.test_num' => $test]);
+        }
+
+        $list = $query->orderBy('score.insert_time desc')->asArray()->all();
+
+        foreach ($list as &$item){
+            $item['status'] = $item['status'] == 1 ? '正在预警' : '预警已解除';
+            $item['show'] = $item['grade'].$item['banji'];
+        }
+        return $list;
+    }
+
+    public static function getWarningAllList($grade,$class,$test,$type,$studentType,$course,$status,$nameStr){
+        $query = Warning::find()->leftJoin('score','score.id = warning.score_id');
+        if ($grade){
+            $query->andWhere(['score.grade' => $grade]);
+        }
+        if ($class){
+            $query->andWhere(['score.banji' => $class]);
+        }
+        if ($test){
+            $query->andWhere(['score.test_num' => $test]);
+        }
+        if ($type){
+            $query->andWhere(['warning.type' => $type]);
+        }
+        if ($studentType){
+            $query->andWhere(['score.type' => $studentType]);
+        }
+        if ($course){
+            $query->andWhere(['warning.dine' => $course]);
+        }
+        if ($status){
+            $query->andWhere(['warning.status' => $status]);
+        }
+        if ($nameStr){
+            $query->andWhere(['like','score.name',$nameStr]);
+        }
+        if ($course){
+            $selectStr = 'score.test_name,score.grade,score.banji,score.name,score.type as studentType,warning.type,warning.content,warning.dine,warning.status';
+        }else{
+            $selectStr = 'score.test_name,score.grade,score.banji,score.name,score.type as studentType,warning.type,warning.content,warning.status';
+        }
+        $list = $query->select($selectStr)
+            ->orderBy('score.insert_time desc')
+            ->asArray()
+            ->all();
+        foreach ($list as &$item){
+            $item['studentType'] = $item['studentType'] == 1 ? '理科' : '文科';
+            $item['type'] = self::$warningType[$item['type']];
+            $item['status'] = $item['status'] == 1 ? '正在预警' : '预警已解除';
+        }
+       return $list;
+    }
+
+    public static function getWarningDetail($id){
+        $warningInfo = Warning::find()->where(['id' => $id])->asArray()->one();
+        $scoreInfo = Score::find()->where(['id' => $warningInfo['score_id']]) -> asArray() -> one();
+
+        $warningInfo['type'] = self::$warningType[$warningInfo['type']];
+        $warningInfo['status'] = $warningInfo['status'] == 1 ? '正在预警' : '预警已解除';
+        $scoreInfo['type'] = $scoreInfo['type'] == 1 ? "理科" : '文科';
+        return [
+            'warningInfo' => $warningInfo,
+            'scoreInfo' => $scoreInfo,
+        ];
+    }
+
+    /**
+     * @return array|\yii\db\ActiveRecord[]
+     * 获取考试列表
+     */
+    public static function getTestList($type,$grade){
+        preg_match('/\d+/',$grade,$arr);
+        $query = Test::find()->leftJoin('grade','grade.id = test.grade_num')
+            ->select('test.test_name,test.test_num,grade.the,type')
+            ->where(['test.status'=>'2','grade.the' => $arr[0]])
+            ->orderBy('test.test_num desc,test.insert_time desc')
+            ->asArray();
+        if ($type){
+            $query->andWhere(['type' => $type]);
+        }
+        return $query->asArray()->all();
     }
 }
