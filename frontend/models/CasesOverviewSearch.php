e3a5c7ac1941ff2b1bc571a03d4dff12c127167b
huangdingbo <hdb378969398>
2019-04-07 20:40:52 +0800

    监控预警api

---
+++ frontend/models/CasesOverviewSearch.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace frontend\models;
+
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+use frontend\models\Cases;
+
+/**
+ * CasesSearch represents the model behind the search form of `frontend\models\Cases`.
+ */
+class CasesOverviewSearch extends Cases
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['id', 'type', 'status'], 'integer'],
+            [['case_num', 'title', 'description', 'pic', 'create_id', 'point_id'], 'safe'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function scenarios()
+    {
+        // bypass scenarios() implementation in the parent class
+        return Model::scenarios();
+    }
+
+    /**
+     * Creates data provider instance with search query applied
+     *
+     * @param array $params
+     *
+     * @return ActiveDataProvider
+     */
+    public function search($params)
+    {
+        $query = Cases::find()->orderBy('status asc');
+        // add conditions that should always apply here
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+            'pagination' => ['pageSize' => 15], //设置分页条数
+        ]);
+
+        $this->load($params);
+
+        if (!$this->validate()) {
+            // uncomment the following line if you do not want to return any records when validation fails
+            // $query->where('0=1');
+            return $dataProvider;
+        }
+
+        // grid filtering conditions
+        $query->andFilterWhere([
+            'id' => $this->id,
+            'type' => $this->type,
+            'status' => $this->status,
+        ]);
+
+
+        $query->andFilterWhere(['like', 'case_num', $this->case_num])
+            ->andFilterWhere(['like', 'title', $this->title])
+            ->andFilterWhere(['like', 'description', $this->description])
+            ->andFilterWhere(['like', 'pic', $this->pic])
+            ->andFilterWhere(['like', 'create_id', $this->create_id])
+            ->andFilterWhere(['like', 'point_id', $this->point_id]);
+
+
+
+        return $dataProvider;
+    }
+}
